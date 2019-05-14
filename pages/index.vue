45f5d2a1ba07a7b9099b4f422bc2f88093a57e07
yuta nishina <y.nishina0509@gmail.com>
2019-05-14 16:18:25 +0900

    NISHINA-5 リンター・フォーマッターの設定をする - ESLintとPrettierを導入 - 'npm run lintfix'を実行してからコミット

--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,15 @@
         My peachy Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey"
-        >GitHub</a>
+          >GitHub</a
+        >
       </div>
     </div>
   </section>
