f8bcb0d34efccb555eae23fea35ade9257c982b0
yuta nishina <y.nishina0509@gmail.com>
2019-05-16 15:52:01 +0900

    - ログイン画面のコンポーネントを作成 - ログイン前にメインのページを開いたらログイン画面にリダイレクトするように

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,15 @@
     メインのページ
   </div>
 </template>
+
+<script>
+export default {
+  beforeMount() {
+    const apiKey = sessionStorage.getItem('apiKey')
+    if (!apiKey) {
+      // apiKeyがセッションストレージに保存されていなければログイン画面にリダイレクトする
+      this.$router.push({ path: '/signin/', query: {} })
+    }
+  }
+}
+</script>
---
+++ pages/signin.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2>
+    ログイン
+  </h2>
+</template>
