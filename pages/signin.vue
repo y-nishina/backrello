5ad26470ef94c152ba8b9642d556934e4b0e0453
yuta nishina <y.nishina0509@gmail.com>
2019-05-24 12:09:04 +0900

    ログイン画面の見た目を一旦修正

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,23 +1,8 @@
 <template>
   <div class="signin">
     <el-row>
-      <h2>疑似ログイン画面</h2>
-      <div>
-        スペースキーとAPIキーを入力して疑似ログインをおこないます
-      </div>
-      <div>
-        ※スペースキーはbacklogのURL（https://XXX.backlog.jp/）のXXXの部分
-      </div>
-      <div>
-        ※APIキーに関しては<a
-          href="https://backlog.com/ja/help/usersguide/personal-settings/userguide2378/"
-          target="_blank"
-          >コチラ</a
-        >
-      </div>
-    </el-row>
-    <el-row>
-      <el-form :model="signinForm" label-width="150px">
+      <h2>ログイン</h2>
+      <el-form :model="signinForm" label-width="150px" label-position="top">
         <el-form-item
           label="スペースキー"
           prop="spaceKey"
@@ -40,6 +25,21 @@
           <el-button type="primary" @click="signin()">ログイン</el-button>
         </el-form-item>
       </el-form>
+    </el-row>
+    <el-row>
+      <div>
+        スペースキーとAPIキーを入力してログインをおこないます
+      </div>
+      <div>
+        ※スペースキーはbacklogのURL（https://XXX.backlog.jp/）のXXXの部分
+      </div>
+      <div>
+        ※APIキーに関しては<a
+          href="https://backlog.com/ja/help/usersguide/personal-settings/userguide2378/"
+          target="_blank"
+          >コチラ</a
+        >
+      </div>
     </el-row>
   </div>
 </template>
@@ -72,9 +72,5 @@ export default {
 .signin {
   max-width: 680px;
   margin: auto;
-  text-align: center;
-  .el-form {
-    margin-top: 60px;
-  }
 }
 </style>
